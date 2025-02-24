cad59b3edcce778de18ea2e8730316bff38d2cf4
20vitorio07 <vitoriomantoani900@gmail.com>
2025-02-24 20:40:56 -0300

    g

--- register.php
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE exercito_irlandes;
-
-USE exercito_irlandes;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
